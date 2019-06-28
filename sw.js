4dea5ef6a4ab7b05b709900aae758df67f8c4da5
Travis CI <sh@signalwerk.ch>
2019-06-28 10:13:17 +0000

    Deploy to GitHub Pages: fd71063097c6365973fa2ab5f03063094592a5e5

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-8d0d4a05c73098cd4150.js","dee0c015cd55734630acb9bd7ed6737c"],["/commons-e4e1a5e349e946049ff1.js","a7a60dc65e3b91f8c1f3e82184fa33e7"],["/index.html","fc6f2aa5f07c681e4918dfba426cbbce"],["/offline-plugin-app-shell-fallback/index.html","583483a3cf0ef592f34443c490422192"]];
+var precacheConfig = [["/app-1801d3fe26f4afa5beda.js","0efe91d360a1110e169a76e93f27e900"],["/commons-e4e1a5e349e946049ff1.js","a7a60dc65e3b91f8c1f3e82184fa33e7"],["/index.html","9226096935346fd24dfe7465d3e7fca7"],["/offline-plugin-app-shell-fallback/index.html","c1f3894b797848aa150fd454a560a658"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
